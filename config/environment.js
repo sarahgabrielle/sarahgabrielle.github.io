4a39775062b32a09c864156286c40a4217837bb9
sarahalpay <sarah.g.alpay@gmail.com>
2018-01-11 14:15:25 +0000

    deleted back end folders

--- config/environment.js
+++
@@ -1,6 +0,0 @@
-const env = process.env.NODE_ENV || 'development';
-const port  = process.env.PORT || 4000;
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost/project-4-${env}`;
-const secret = process.env.AUTH_SECRET || 'hYgs^=?>@qrTfxLp';
-
-module.exports = { port, dbURI, secret, env };
